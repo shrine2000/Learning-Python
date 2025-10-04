caf234eb70e8fbdece08025963d061b5be8b0762
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2025-10-04 11:33:37 +0530

    Add context manager example with resource management

---
+++ src/context_managers/example1.py
@@ -0,0 +1,12 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def managed_resource(name: str) -> None:
+    print(f"Resource {name} acquired")
+    yield f"resource-{name}"
+    print(f"Resource {name} released")
+    
+
+with managed_resource("A") as resource:
+    print(f"Using {resource}")
