647c88e54e46fbb6c3893243a1662b464d105880
Shrine Sabu <39656631+shrine2000@users.noreply.github.com>
2026-04-25 19:35:34 +0530

    Removed Validations

--- src/api_calls/requests_example.py
+++ src/api_calls/requests_example.py
@@ -1,15 +1,15 @@
 import requests
-from pydantic import BaseModel
+from dataclasses import dataclass
 
 
-class Joke(BaseModel):
+@dataclass
+class Joke:
     id: int
     type: str
     setup: str
     punchline: str
 
 
-response = requests.get("https://official-joke-api.appspot.com/jokes/1", timeout=5)
-data = response.json()
+data = requests.get("https://official-joke-api.appspot.com/jokes/1", timeout=5).json()
 joke = Joke(**data)
 print(joke)
